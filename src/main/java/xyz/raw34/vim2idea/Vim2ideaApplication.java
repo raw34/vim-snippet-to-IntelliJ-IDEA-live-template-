1210edf92ec025977fbc4bfac3180fa470846573
raw34 <raw34@sina.com>
2020-05-21 15:08:09 +0800

    update

--- src/main/java/xyz/raw34/vim2idea/Vim2ideaApplication.java
+++ src/main/java/xyz/raw34/vim2idea/Vim2ideaApplication.java
@@ -1,18 +1,22 @@
 package xyz.raw34.vim2idea;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Vim2ideaApplication implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(Vim2ideaApplication.class);
 
     @Override
     public void run(String... args) throws Exception{
-        System.out.println("Running Spring Boot Application!!!");
+        logger.info("Running Spring Boot Application!!!");
+
         if (args.length > 0 ) {
             for (String arg : args) {
-                System.out.println("Command line argument: " + arg);
+                logger.info("Command line argument: {}", arg);
             }
         }
     }
